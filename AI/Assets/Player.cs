16f02f6b83558780528f195675826311fff059ac
Rory <rd553@york.ac.uk>
2017-02-24 12:15:06 +0000

    Death conditions fixed

--- AI/Assets/Player.cs
+++ AI/Assets/Player.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class Player : Character {
 
 	public UnityEngine.UI.Text hpText;
+	public UnityEngine.UI.Text deadText;
+	private bool dead;
 
 	// Use this for initialization
 	void Start () {
 		base.Initialize ();
 		updateUI ();
+		dead = false;
+		deadText.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (dead && Input.GetKeyDown (KeyCode.Space)) {
+			Application.Quit ();
+		}
 	}
 
 	void OnTriggerEnter(Collider c){
+
+
 
 		EnemyShot b = c.gameObject.GetComponent<EnemyShot> ();
 
@@ -25,14 +33,29 @@ public class Player : Character {
 		if(b!=null){
 			currentHealth -= b.damage;
 			updateUI();
-			/**if (currentHealth <= 0) {
-				BeDestroyed ();
-			}*/
+			if (currentHealth <= 0) {
+
+				Die ();
+
+
+			}
 
 
 
 
 		}
+
+		if (c.gameObject.layer == 13) {
+			Die ();
+		}
+	}
+
+	void Die(){
+		currentHealth = 0;
+		Destroy(gameObject.GetComponent<PlayerMovement> ());
+		updateUI ();
+		dead = true;
+		deadText.enabled = true;
 	}
 
 	void updateUI(){
